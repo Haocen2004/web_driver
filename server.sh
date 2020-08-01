d6b677be660b6f725463bce35ccaed81564322d1
Haocen2004 <haocen.minecraft@qq.com>
2020-08-01 16:45:30 +0800

    update

--- server.sh
+++ server.sh
@@ -3,4 +3,5 @@
 wget https://raw.githubusercontent.com/Haocen2004/web_driver/master/server.zip -O server.zip
 unzip server.zip
 cd server
-ls
+wget https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar -O vserver.jar
+screen -R server java -Xmx3G -server -jar server.jar nogui
