ff6ab38f0f5398f9d775bb2df7b3c6f543da00cc
Haocen2004 <haocen.minecraft@qq.com>
2020-08-01 18:11:10 +0800

    first stable version

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#yum install java screen -y 
+yum install java screen -y 
 wget https://raw.githubusercontent.com/Haocen2004/web_driver/master/server.zip -O server.zip
 unzip server.zip
 cd server
 wget https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar -O vserver.jar
-screen -R server java -Xmx3G -server -jar server.jar nogui
+echo eula=true>eula.txt
