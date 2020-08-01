4aa436227e3bc75ff673ecabb9783a12350b3e09
Haocen2004 <haocen.minecraft@qq.com>
2020-08-01 16:34:11 +0800

    add some test file

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum install java screen -y 
-wget 
-unzip
+#yum install java screen -y 
+wget https://raw.githubusercontent.com/Haocen2004/web_driver/master/server.zip -O server.zip
+unzip server.zip
 cd server
-java -
+ls
