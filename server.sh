a6c9b7276d73be556b36ed3155b83dd1f2f44e55
Haocen2004 <haocen.minecraft@qq.com>
2020-08-02 18:37:44 +0800

    second test

--- server.sh
+++ server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-yum install java screen -y 
-wget https://raw.githubusercontent.com/Haocen2004/web_driver/master/server.zip -O server.zip
+yum install java screen git -y
+git clone https://gitee.com/Haocen2004/web_driver.git
+cd web_driver
 unzip server.zip
-cd server
-wget https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar -O vserver.jar
-echo eula=true>eula.txt
