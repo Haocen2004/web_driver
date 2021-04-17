404038d8d1d7ff997ccb7aa4124e37985dbada63
Haocen2004 <haocen.minecraft@qq.com>
2021-04-17 20:49:30 +0800

    Update exthmuiSetup.sh

--- exthmuiSetup.sh
+++ exthmuiSetup.sh
@@ -8,23 +8,19 @@ sudo chmod a+x /bin/repo
 sudo fdisk -l
 mkdir exthmui
 echo "开始挂载磁盘..."
-echo -n "请输入磁盘名："
-read disk_name
+read -p "请输入磁盘名：" disk_name
+echo "参考指令 n -> enter -> enter -> enter -> w"
 sudo fdisk ${disk_name}
-n
-
-
-
-w
+echo "格式化分区中..."
+echo "额外信息处请直接回车"
 sudo mkfs.ext4 ${disk_name}1
 sudo mount ${disk_name}1 exthmui
-echo "磁盘挂载完成,请手动设置git参数"
-read -p "git config --global user.name " git_username
-read -p "git config --global user.email " git_email
-git config --global user.name ${git_username}
-git config --global user.email ${git_email}
-cd exthmui
+git config --global user.name "Hao_cen"
+git config --global user.email "haocen.minecraft@gmail.com"
+sudo chmod 777 exthmui
+cd exthmui/
 echo "repo初始化..."
-repo init -u https://github.com/exthmui/android.git -b exthm-11
+repo init -u https://github.com/exthmui/android.git -b exthm-11 -q
 echo "开始下载安卓源码..."
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+echo "初始化完毕."
